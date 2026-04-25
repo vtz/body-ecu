83e7a765b6630c1b9e1701fd64aaea55fc8819bf
Vinicius Zein <vtzein@gmail.com>
2026-04-25 11:56:24 -0400

    chore: stop tracking build_h755 artifacts

--- build_h755/openbsw/libs/3rdparty/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/vzein/Projects/spikes/openbsw/libs/3rdparty/googletest
-# Build directory: /Users/vzein/Projects/spikes/body-ecu/build_h755/openbsw/libs/3rdparty/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
